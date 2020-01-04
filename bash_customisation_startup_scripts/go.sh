51fa801231cfc645381122f09109749965833ec7
Michael Isidro <isidro.michael@gmail.com>
2020-01-05 01:21:22 +1100

    Make GOPATH initialisation clearer

--- bash_customisation_startup_scripts/go.sh
+++ bash_customisation_startup_scripts/go.sh
@@ -4,9 +4,14 @@ echo -e "${GRAY}Running ${BASH_SOURCE}"
 
 ###############################################################################
 # Set GOPATH and path
-mkdir -p ${HOME}/goworkspace/src
+mkdir -p "${HOME}/goworkspace/src"
+
+# The default conventional GOPATH to store downloaded third party libraries
 export GOPATH="${HOME}/go"
-export PATH=$PATH:"$(go env GOPATH)/bin"
-export GOPATH=${GOPATH}:${HOME}/goworkspace
+export PATH=${PATH}:"${GOPATH}/bin"
+
+# Have a separate workspace to install your own source code in that is free of
+# `go get ...` installed libraries
+export GOPATH="${GOPATH}:${HOME}/goworkspace"
 
 ###############################################################################
